a753decdf74f11968c4024d2ee864fe1911620a0
Tauri93 <richaudfranck93@gmail.com>
2022-11-30 22:26:18 +0100

    ca va mieux

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello from code4felix.com fais chier!')
+  res.send('Hello from code4felix.com fais  moins chier!')
 })
 
 app.listen(port, () => {
